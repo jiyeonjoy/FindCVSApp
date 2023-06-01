1956219bfa632fa2af708a7e1205dd690229237d
최지연 <choi02647@naver.com>
2023-06-02 01:07:07 +0900

    added entities

---
+++ FindCVSApp/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  FindCVSApp
+//
+//  Created by Jiyeon Choi on 2023/06/02.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ FindCVSApp/Entities/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  FindCVSApp
+//
+//  Created by Jiyeon Choi on 2023/06/02.
+//
+
+import Foundation
+
+struct LocationData: Decodable {
+    let documents: [KLDocument]
+}
