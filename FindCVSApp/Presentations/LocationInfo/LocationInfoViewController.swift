b88ce2ffcb1981c03a5e520d388bacc7168d79b0
최지연 <choi02647@naver.com>
2023-06-02 01:13:21 +0900

    deleted main storyboard

---
+++ FindCVSApp/Presentations/LocationInfo/LocationInfoViewController.swift
@@ -0,0 +1,10 @@
+//
+//  LocationInfoViewController.swift
+//  FindCVSApp
+//
+//  Created by Jiyeon Choi on 2023/06/02.
+//
+
+import UIKit
+
+class LocationInformationViewController: UIViewController {}
---
+++ FindCVSApp/Presentations/LocationInfo/LocationInformationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LocationInformationViewModel.swift
+//  FindCVSApp
+//
+//  Created by Jiyeon Choi on 2023/06/02.
+//
+
+import Foundation
+
+struct LocationInformationViewModel {
+    
+}
